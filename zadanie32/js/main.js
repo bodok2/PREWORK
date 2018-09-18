155ec2fb7acfc0a6e98215dd6284122280a5bf44
Bogdan Kalinowski <bodok2@gmail.com>
2018-09-18 16:27:27 +0200

    wersja druga

--- zadanie32/js/main.js
+++ zadanie32/js/main.js
@@ -2,8 +2,8 @@ var num1 = 2;
 num1++;
 var num2 = 5;
 num2++;
-function myfunction() {
+function myfunction(num1, num2) {
     var result = (num1 + num2);
     return result;
 }
-myfunction();
+myfunction(num1,num2);
