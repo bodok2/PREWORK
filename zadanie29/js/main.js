048d6b848d39f6a40a03b48ca5e1ae3428e4d7d8
Bogdan Kalinowski <bodok2@gmail.com>
2018-09-19 11:19:57 +0200

    wersja trzecia

--- zadanie29/js/main.js
+++ zadanie29/js/main.js
@@ -1,6 +1,6 @@
 var liczbaPierwsza = 8;
 var liczbaDruga = 2;
 function myfunction(liczbaPierwsza, liczbaDruga) {
-    var suma = (liczbaPierwsza + liczbaDruga);
+    var suma = liczbaPierwsza + liczbaDruga;
     return suma; }
 myfunction(liczbaPierwsza, liczbaDruga);
