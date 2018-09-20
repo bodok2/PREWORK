eb2aa1dad8ebf9d0062bdafdffbbb2b0b1ebf435
Bogdan Kalinowski <bodok2@gmail.com>
2018-09-20 14:50:17 +0200

    wersja pierwsza

---
+++ zadanie35/main.js
@@ -0,0 +1,14 @@
+//function myFunction(n) {
+//    if (n >= 100) {console.log("Liczba znajduje się w przedziale");
+//}
+//    else if (n >= 200) {console.log("Liczba nie znajduje się w przedziale");
+//}
+//         else {console.log("Liczba nie znajduje się w przedziale");}}
+//myFunction(8);
+
+
+function myFunction(n) {
+    if (n >= 100 && n <= 200) {console.log("Liczba znajduje się w przedziale");
+}
+         else {console.log("Liczba nie znajduje się w przedziale");}}
+myFunction(201);
