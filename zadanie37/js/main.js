0bd56deb74e149929fe1bfc5ec2b2c9e9bbffd99
Bogdan Kalinowski <bodok2@gmail.com>
2018-09-21 09:46:56 +0200

    wersja pierwsza

---
+++ zadanie37/js/main.js
@@ -0,0 +1,11 @@
+
+    function myFunction1() {
+    document.querySelector("p").innerHTML = "Akademia 108";
+    
+    }
+
+
+    function myFunction2() {
+    document.querySelector("p").innerHTML = "";
+    
+    }
