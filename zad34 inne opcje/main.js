8f1390d7cc4888cbd23e97adeea682574b6d9763
Bogdan Kalinowski <bodok2@gmail.com>
2018-09-27 22:16:15 +0200

    dodanie parametrów

---
+++ zad34 inne opcje/main.js
@@ -0,0 +1,73 @@
+var x = "Uwielbiam JavaScript";
+var 20;
+var y = "Jestem świetnym programistą";
+var 27;
+function myFunction(a,b) 
+
+{   
+
+    if  (x>y) 
+    
+    {
+        console.log("Jestem świetnym programistą");
+    }
+    
+    else
+{console.log("Uwielbiam JavaScript");
+}}
+myFunction();
+
+
+
+//var x = "Uwielbiam JavaScript";
+//
+//var y = "Jestem świetnym programistą";
+//
+//function myFunction() 
+//
+//{   
+//
+//    if  (x.length > y.lenght) 
+//    
+//    {
+//        console.log("true");
+//    }
+//    
+//    else
+//{console.log("false");
+//}}
+//myFunction();
+//
+
+
+
+//var x = "Uwielbiam JavaScript";
+//
+//var y = "Jestem świetnym programistą";
+//
+//function myFunction(x,y) 
+//
+//if  (x.length > y.lenght) 
+//    
+//    {
+//        console.log("true");
+//    }
+//
+//    else{console.log("false");
+//}}
+//myFunction();
+
+
+
+//{   
+//
+//    if  (x.length > y.lenght) 
+//    
+//    {
+//        console.log("true");
+//    }
+//    
+//    else
+//{console.log("false");
+//}}
+//myFunction();
